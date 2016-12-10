efad0bb4b89e550f31ab852bb60935fe5dc46667
Gabriel Lanata <gabriellanata@gmail.com>
2016-12-10 13:49:54 -0500

    Json PProcessing with error detection

--- Sources/JsonProcess.swift
+++ Sources/JsonProcess.swift
@@ -21,3 +21,42 @@ public func JsonProcess(_ loadedValue: String) throws -> Json {
     return try Json.parse(string: loadedValue)
 }
 
+public func JsonProcessWithError(_ loadedValue: Data) throws -> Json {
+    return try Json.parse(data: loadedValue).processError()
+}
+
+public func JsonProcessWithError(_ loadedValue: [UInt8]) throws -> Json {
+    return try Json.parse(bytes: loadedValue).processError()
+}
+
+public func JsonProcessWithError(_ loadedValue: String) throws -> Json {
+    return try Json.parse(string: loadedValue).processError()
+}
+
+public extension Json {
+    
+    @discardableResult
+    func processError() throws -> Json {
+        if let error = self["error"].bool, error == true {
+            let message = self["message"].string ?? "Unknown Error"
+            throw JsonInnerError.containsError(message)
+        }
+        if let error = self["error"].string {
+            throw JsonInnerError.containsError(error)
+        }
+        if let error = self["error"]["message"].string {
+            throw JsonInnerError.containsError(error)
+        }
+        return self
+    }
+    
+}
+
+public enum JsonInnerError: Error, CustomStringConvertible {
+    case containsError(String)
+    public var description: String {
+        switch self {
+        case .containsError(let message): return message
+        }
+    }
+}
